40993f456cae326a5a9f468eea80e51eededa7c4
alielrayes <engali7assan@yahoo.com>
2022-11-02 20:33:23 -0700

    lesson8

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIN6L3M6dGBm7ltodXyg9EmGGWt64_Sns",
+  authDomain: "react2-lesson8.firebaseapp.com",
+  projectId: "react2-lesson8",
+  storageBucket: "react2-lesson8.appspot.com",
+  messagingSenderId: "910926898717",
+  appId: "1:910926898717:web:290c914dc006219a7a08f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
