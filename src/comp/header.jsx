cd64fc32cfc3d2ed4116023518a72753715f54c3
alielrayes <engali7assan@yahoo.com>
2022-11-06 18:37:25 -0800

    lesson20

--- src/comp/header.jsx
+++ src/comp/header.jsx
@@ -19,9 +19,9 @@ const Header = () => {
   return (
     <div className="myheader">
 
-      <header className="hide-when-mobile ali">
+      <header className="hide-when-mobile ali ">
         <h1>
-          <Link to="/">c4a.dev</Link>
+          <a className="logo" href="https://c4a.dev/" >c4a.dev</a>
         </h1>
         {/* <button
           onClick={() => {
@@ -101,75 +101,9 @@ const Header = () => {
 
 
 
-
-      <header className="show-when-mobile ali">
-        <h1>c4a.dev</h1>
-        <label className="absolute" htmlFor="burger">
-          <i className="fas fa-bars" />
-        </label>
-        <input id="burger" type="checkbox" />
-        <div className="show-on-click">
-          <div className="main-div">
-            <label htmlFor="html">
-              HTML <i className="fas fa-plus" />
-            </label>
-            <input id="html" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <NavLink to="/html">Full Course</NavLink>
-              </li>
-              <li>
-                <a href="">Crash Course</a>
-              </li>
-              <li>
-                <a href="">learn in 1h</a>
-              </li>
-            </ul>
-          </div>
-          <div className="main-div">
-            <label htmlFor="css">
-              CSS <i className="fas fa-plus" />
-            </label>
-            <input id="css" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <NavLink to="/css">Full Course</NavLink>
-              </li>
-              <li>
-                <a href="">CSS Examples</a>
-              </li>
-              <li>
-                <label className="mini-projects" htmlFor="mini">
-                  mini projects <i className="fas fa-plus" />
-                </label>
-                <input id="mini" type="checkbox" />
-                <ul className="sub-sub-div">
-                  <li>
-                    <a href="">project 1</a>
-                  </li>
-                  <li>
-                    <a href="">project 2</a>
-                  </li>
-                  <li>
-                    <a href="">project 3</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          <div className="main-div">
-            <label htmlFor="js">
-              JavaScript <i className="fas fa-plus" />
-            </label>
-            <input id="js" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <NavLink to="/javascript">coming soon🔥</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
+ 
+  
+  
     </div>
   );
 };
